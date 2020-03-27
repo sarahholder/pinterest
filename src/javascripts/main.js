9fa73380764d413979871fbb076e5d0d722b8338
Sarah Holder <sr.holder@yahoo.com>
2020-03-27 17:59:12 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
 
 import authData from './helpers/data/authData';
-import auth from './components/auth/auth';
+import home from './components/home/home';
 import myNavbar from './components/myNavbar/myNavbar';
 import 'bootstrap';
 import '../styles/main.scss';
@@ -10,8 +10,7 @@ import '../styles/main.scss';
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   myNavbar.logoutEvent();
-  auth.loginButton();
+  home.loginButton();
   authData.checkLoginStatus();
 };
-
 init();
