4c9b6f684098725c26e00cbcfebddcc419a2bc25
Sarah Holder <sr.holder@yahoo.com>
2020-03-27 16:00:02 -0500

    created proper spacing on navbar

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,26 +1,29 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+
 import boards from '../../components/boards/boards';
 
 const authDiv = $('#auth');
-// const homeDiv = $('#home');
 const boardsDiv = $('#boards');
 const logoutButton = $('#navbar-logout-button');
 const navHeadingDiv = $('#navHeading');
+const navHeadingBoardsDiv = $('#navHeadingBoards');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       authDiv.addClass('hide');
+      boardsDiv.removeClass('hide');
       logoutButton.removeClass('hide');
       navHeadingDiv.addClass('hide');
-      boardsDiv.removeClass('hide');
+      navHeadingBoardsDiv.removeClass('hide');
       boards.showBoards();
     } else {
       authDiv.removeClass('hide');
-      logoutButton.addClass('hide');
       boardsDiv.addClass('hide');
       navHeadingDiv.removeClass('hide');
+      navHeadingBoardsDiv.addClass('hide');
+      logoutButton.addClass('hide');
     }
   });
 };
