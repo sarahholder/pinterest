727495c8093c2618ced0af8e57fcbdeddf158313
Sarah Holder <sr.holder@yahoo.com>
2020-03-28 22:25:25 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -7,11 +7,15 @@ import myNavbar from './components/myNavbar/myNavbar';
 import 'bootstrap';
 import '../styles/main.scss';
 
+const events = () => {
+  $('body').on('click', '.board', viewBoardPins);
+};
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   myNavbar.logoutEvent();
   home.loginButton();
   authData.checkLoginStatus();
+  events();
 };
 init();
