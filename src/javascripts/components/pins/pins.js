5bd72a02f32b38d08734c0b95477fe735b1f79e9
Sarah Holder <sr.holder@yahoo.com>
2020-04-10 21:36:34 -0500

    added forms for new pin/boards

--- src/javascripts/components/pins/pins.js
+++ src/javascripts/components/pins/pins.js
@@ -25,15 +25,37 @@ const printPins = (boardId) => {
     .then((response) => {
       const selectedPins = response;
       let domString = '';
-      domString += '<i class="m-3 goBack far fa-arrow-alt-circle-left fa-2x"></i>';
+      domString += `
+      <i id="floater" class="m-3 goBack far fa-arrow-alt-circle-left float-left fa-2x"></i>
+      <div class="accordion" id="accordionExample">
+        <h2 class="mb-0">
+          <button class="btn" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <i class="fas fa-plus-circle"></i> add Pin</button>
+        </h2>
+      </div>
+      <div class="form m-2">
+        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+          <div class= "text-left row d-flex flex-wrap justify-content-center">
+            <div class="form-group col-md-4">
+              <label class="" for="add-pin-title">Pin Title:</label>
+              <input type="text" class="form-control" id="add-pin-title" placeholder="add pin title here">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="add-pin-img">Pin Image:</label>
+              <input type="text" class="form-control" id="add-pin-img" placeholder="add pin image link here">
+            </div>
+          </div>
+          <button type="submit" class="btn btn-danger">Submit</button>
+        </div>
+      </div>`;
       domString += '<div class="card-columns justify-content-center ml-5 mr-5">';
       selectedPins.forEach((selectedPin) => {
         domString += `<div id="${selectedPin.id}" class="card">`;
         domString += `  <img src="${selectedPin.imageUrl}" class="card-img-bottom alt="...">`;
         domString += '<div class="card-body">';
-        domString += '  <div class="row">';
-        domString += `    <p class="col-9 p-0 m-0 card-description">${selectedPin.name}</p>`;
-        domString += '    <div class= "col-3 p-0 m-0">';
+        domString += '  <div class="row ">';
+        domString += `    <p class="col-10 p-0 m-0 card-description">${selectedPin.name}</p>`;
+        domString += '    <div class= "col-2 p-0 m-0">';
         domString += `      <button id="${selectedPin.boardId}" class="board-id delete-btn"><i class="delete-pin far fa-trash-alt"></i></button>`;
         domString += '    </div>';
         domString += '  </div>';
